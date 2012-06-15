626035bf94d282c728802d1ffe5cbcd7a923aa24
lucianoizquierdo <lucianoizquierdo@gmail.com>
2012-06-15 17:03:49 +0000

    + 15/06/2012 Estoy agregando la seguridad de la aplicacion.

---
+++ Apce/Apce-parent/Servicio/src/main/java/com/apce/servicio/servicio/Impl/SecurityServiceImpl.java
@@ -0,0 +1,26 @@
+package com.apce.servicio.servicio.Impl;
+
+import java.util.List;
+
+import com.apce.modelo.Permiso;
+import com.apce.modelo.Sucursal;
+import com.apce.modelo.Usuario;
+import com.apce.persistencia.interfaz.BaseDAO;
+import com.apce.servicio.servicoInterfaz.SecurityService;
+
+public class SecurityServiceImpl implements SecurityService {
+
+	Permiso permisoDao;
+	
+	@Override
+	public void setDao(BaseDAO dao) {
+		this.permisoDao = (Permiso)dao;
+	}
+
+	@Override
+	public List getListControllersAccesibles(Usuario us, Sucursal suc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
