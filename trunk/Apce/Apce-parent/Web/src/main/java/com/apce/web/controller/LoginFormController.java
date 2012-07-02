efba6b3a222a552b640d390680f510e6449f59cd
lucianoizquierdo@gmail.com <lucianoizquierdo@gmail.com@b46a85e8-a850-a960-736c-ff68b8ff0db0>
2012-07-02 21:01:00 +0000

    + 02/07/2012  Agregue las validaciones para spring con validation.xml y validator-rules.xml

--- trunk/Apce/Apce-parent/Web/src/main/java/com/apce/web/controller/LoginFormController.java
+++ trunk/Apce/Apce-parent/Web/src/main/java/com/apce/web/controller/LoginFormController.java
@@ -1,5 +1,7 @@
 package com.apce.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -16,7 +18,18 @@ public class LoginFormController extends SimpleFormController {
         return new ModelAndView(new RedirectView("main.do"));
         
     }
-    
+
+    /*
+	@Override
+	protected Object formBackingObject(HttpServletRequest request)
+			throws Exception {
+		// TODO Auto-generated method stub
+		LoginForm loginForm = (LoginForm) super.formBackingObject(request);
+		loginForm.setUsuario("peron");
+		return loginForm;
+		//return super.formBackingObject(request);
+	}
+    */
     
     
 }
