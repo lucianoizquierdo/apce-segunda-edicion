e396662870e8612a667b05f58a86570ebfef27de
lucianoizquierdo@gmail.com <lucianoizquierdo@gmail.com@b46a85e8-a850-a960-736c-ff68b8ff0db0>
2012-07-04 20:36:28 +0000

    + 04/07/2012  Algunos cambios en general

--- trunk/Apce/Apce-parent/Persistencia/src/main/java/com/apce/persistencia/hibernate/BaseDaoImpl.java
+++ trunk/Apce/Apce-parent/Persistencia/src/main/java/com/apce/persistencia/hibernate/BaseDaoImpl.java
@@ -1,7 +1,5 @@
 package com.apce.persistencia.hibernate;
 
-import org.hibernate.SessionFactory;
-import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.apce.persistencia.interfaz.BaseDAO;
