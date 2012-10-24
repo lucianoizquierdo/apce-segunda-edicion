d4e11e5d513d815ef510626169749ab5aa9929eb
lucianoizquierdo <lucianoizquierdo@b46a85e8-a850-a960-736c-ff68b8ff0db0>
2012-10-24 21:32:48 +0000

    estoy armando la lista de mateiras primas

---
+++ trunk/Apce/Apce-parent/Persistencia/src/main/resources/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO materias_primas(
+            codigo_materia_prima, tipo, tamanio, color, descripcion, kilos, 
+            litros, activa)
+    VALUES (18, 'rollo','40 cm','rojo', 'es un rollo', 50,
+            20,true);
